230136c74125878a824b545bbed71c794529e841
AlinaBielska <alinkabelka5@gmail.com>
2023-03-31 16:30:08 +0200

    dodanie fetch, funkcja fetchCountries(name)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/styles.css';
 
+const input = document.querySelector("#search-box");
+const countryList = document.querySelector(".country-list");
+const countryInfo = document.querySelector(".country-info");
 const DEBOUNCE_DELAY = 300;
+
+input.addEventListener("input", () => {
+    fetchCountries(name)
+        .then((countries) => renderCountryList(countries))
+        .catch((error) => console.log(error));
+});
+
+function fetchCountries(name) {
+    return fetch("https://restcountries.com/v3.1/name/{name}")
+        .then((response) => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+        return response.json();
+    });
+};
